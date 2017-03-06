a2b5532900cbb43f3713cff934f392f6ce5517ce
JarrodThorwardson <thorwardsonjl01@mounties.mansfield.edu>
2017-03-06 14:31:07 -0500

    Adjusting variables.
    Made numeric variable into strings, for concatentation.
    First version of a formatted string, for display.

--- HipchatBuildNotices.rb
+++ HipchatBuildNotices.rb
@@ -1,24 +1,24 @@
 class HipchatBuildNotices
   require "HipchatNotifications"
 
-  def self.buildStart(authToken, endpointURL, message)
+  def self.buildStart(authToken, endpointURL, message, buildID)
     pushNotification = "false"
     color = "green"
-    noticePrefix = "NOTICE: "
+    noticePrefix = "NOTICE: Build " + buildID + " started.<br>"
     HipchatNotifications.privateMessage(authToken,endpointURL,message,color, pushNotification, noticePrefix)
   end
 
-  def self.buildFinish(authToken, endpointURL, message)
+  def self.buildFinish(authToken, endpointURL, message, buildID)
     pushNotification = "false"
     color = "green"
-    noticePrefix = "NOTICE: "
+    noticePrefix = "NOTICE: Build " + buildID + " finished.<br>"
     HipchatNotifications.privateMessage(authToken,endpointURL,message,color, pushNotification, noticePrefix)
   end
 
-  def self.buildError(authToken, endpointURL, message)
+  def self.buildError(authToken, endpointURL, message, buildID)
     pushNotification = "true"
     color = "red"
-    noticePrefix = "ERROR: "
+    noticePrefix = "ERROR: Build " + buildID + " failed.<br>"
     HipchatNotifications.privateMessage(authToken,endpointURL,message,color, pushNotification, noticePrefix)
   end
 
--- firstPost.rb
+++ firstPost.rb
@@ -5,17 +5,37 @@ require "HipchatBuildNotices"
 token = File.read("credentials.txt") #one-line file containing the personal auth token
 
 # /v2/user/{id_or_email}/message is the API endpoint for a private message, requiring a different scope from rooms.
-pmurl = File.read("pmUser.txt") #one-line file containing the target URL, for API private message to user
+# pmurl = File.read("pmUser.txt") #one-line file containing the target URL, for API private message to user
 testurl = File.read("host.txt")
+
+vmBuildID = "9001"
+procQuant = "1"
+memQuant = "8 GB"
+OS = "CentOS"
+storage = "200 GB"
+transit = "1 TB"
+customer = "Taylor Swift"
+engineerContact = "Wait, Tay has an account with us?"
+currentTime = Time.now
+
+verboseTestMessage = ["<br> VM Resources <br> ",
+  "Cores: " + procQuant + "<br>", 
+  "Memory: " + memQuant + "<br>",
+  "OS: " + OS + "<br>",
+  "Storage: " + storage + "<br>",
+  "Transit: " + transit + "<br>",
+  "Customer: " + customer + "<br>",
+  "Account Engineer: " + engineerContact + "<br>",
+  "Build started at: " + currentTime.inspect + "<br>"].join
 
 testMessage = "This is an automated message testing color."
 finishMessage = "This is what it looks like when a build finishes."
 errorMessage = "This is what happens when something goes wrong."
 
 begin
- # HipchatBuildNotices.buildStart(token, pmurl, testMessage)
- # HipchatBuildNotices.buildFinish(token, pmurl, finishMessage)
-  HipchatBuildNotices.buildError(token, testurl, errorMessage)
+  HipchatBuildNotices.buildStart(token, testurl, verboseTestMessage, vmBuildID)
+  HipchatBuildNotices.buildFinish(token, testurl, verboseTestMessage, vmBuildID)
+  HipchatBuildNotices.buildError(token, testurl, verboseTestMessage, vmBuildID)
 rescue RestClient::ExceptionWithResponse => e
   puts(e.response)
 end
