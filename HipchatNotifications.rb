1a55fd5f817acdee0cdff8bcb6b4d502a4ea7c8e
JarrodThorwardson <thorwardsonjl01@mounties.mansfield.edu>
2017-03-09 20:23:47 -0500

    Class refactoring.
    Refactoring into a single class, with a design based around
    instantiation rather than invoking static methods.
    TODO: Support more convenient messaging of specific users, and selecting
    rooms to post notifications to.

--- HipchatNotifications.rb
+++ HipchatNotifications.rb
@@ -1,17 +1,50 @@
 class HipchatNotifications
   require "rest-client"
   require "json"
+  
+  def initialize(token)
+    authToken = "Bearer " + token
+    @headers = { 'Authorization' => authToken, :content_type => 'application/json' }
+  end
 
-  def self.privateMessage(authToken, endpointURL, message, color, pushNotification, messagePrefix)
-    authString = "Bearer " + authToken
+  def privateMessage(endpointURL, message, color, pushNotification, messagePrefix)
     testPost = "<span style=\"color:"+color+"\">" + messagePrefix + "</span>" + message
     pmpayload = JSON.generate({ 'message_format'=> 'html', 'notify'=> pushNotification, 'message'=> testPost })
-    headers = { 'Authorization' => authString, :content_type => 'application/json' }
     begin
-      RestClient.post(endpointURL, pmpayload, headers)
+      RestClient.post(endpointURL, pmpayload, @headers)
     rescue RestClient::ExceptionWithResponse => e
       puts(e.response)
     end
+  end
+
+  def getFoo(endPointURL)
+  kk = endPointURL
+    begin
+    RestClient.get(kk, @headers)
+    rescue RestClient::ExceptionWithResponse => e
+      puts(e.response)
+    end
+  end
+
+  def buildStart(endpointURL, message, buildID)
+    pushNotification = "false"
+    color = "green"
+    noticePrefix = "NOTICE: Build " + buildID + " started.<br>"
+    self.privateMessage(endpointURL,message,color, pushNotification, noticePrefix)
+  end
+
+  def buildFinish(endpointURL, message, buildID)
+    pushNotification = "false"
+    color = "green"
+    noticePrefix = "NOTICE: Build " + buildID + " finished.<br>"
+    self.privateMessage(endpointURL,message,color, pushNotification, noticePrefix)
+  end
+
+  def buildError(endpointURL, message, buildID)
+    pushNotification = "true"
+    color = "red"
+    noticePrefix = "ERROR: Build " + buildID + " failed.<br>"
+    self.privateMessage(endpointURL,message,color, pushNotification, noticePrefix)
   end
 
 end
--- firstPost.rb
+++ firstPost.rb
@@ -1,4 +1,4 @@
-require "HipchatBuildNotices"
+require "HipchatNotifications"
 
 # A personal auth token will result in the posts coming from your account.
 # https://encoretech.hipchat.com/account/api is the page where you generate such a token.
@@ -7,6 +7,8 @@ token = File.read("credentials.txt") #one-line file containing the personal auth
 # /v2/user/{id_or_email}/message is the API endpoint for a private message, requiring a different scope from rooms.
 # pmurl = File.read("pmUser.txt") #one-line file containing the target URL, for API private message to user
 testurl = File.read("host.txt")
+
+getTestURL = File.read("hist.txt")
 
 vmBuildID = "9001"
 procQuant = "1"
@@ -32,10 +34,13 @@ testMessage = "This is an automated message testing color."
 finishMessage = "This is what it looks like when a build finishes."
 errorMessage = "This is what happens when something goes wrong."
 
+testObjectGet = HipchatNotifications.new(token)
+
 begin
  # HipchatBuildNotices.buildStart(token, testurl, verboseTestMessage, vmBuildID)
  # HipchatBuildNotices.buildFinish(token, testurl, verboseTestMessage, vmBuildID)
-  HipchatBuildNotices.buildError(token, testurl, verboseTestMessage, vmBuildID)
+ # HipchatBuildNotices.buildError(token, testurl, verboseTestMessage, vmBuildID)
+  puts(testObjectGet.getFoo(getTestURL))
 rescue RestClient::ExceptionWithResponse => e
   puts(e.response)
 end
