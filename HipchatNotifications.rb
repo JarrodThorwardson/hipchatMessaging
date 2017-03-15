5b0aaf96a81a3ed7843216aa69de42d349721303
JarrodThorwardson <thorwardsonjl01@mounties.mansfield.edu>
2017-03-15 16:39:31 -0400

    Changes supporting custom messages.
    In order to support greated flexibility in recipient, altering how the
    url called was formed was necessary.
    Also, some changes related to retrieving a list of rooms to send to.

--- HipchatNotifications.rb
+++ HipchatNotifications.rb
@@ -1,50 +1,40 @@
 class HipchatNotifications
   require "rest-client"
   require "json"
-  
+
   def initialize(token)
     authToken = "Bearer " + token
     @headers = { 'Authorization' => authToken, :content_type => 'application/json' }
   end
 
-  def privateMessage(endpointURL, message, color, pushNotification, messagePrefix)
-    testPost = "<span style=\"color:"+color+"\">" + messagePrefix + "</span>" + message
-    pmpayload = JSON.generate({ 'message_format'=> 'html', 'notify'=> pushNotification, 'message'=> testPost })
+  def sendMessage(endpointURL, recipient, message)
+    url = endpointURL + recipient + "/message"
+    testPost = message
+    payload = JSON.generate({ 'message_format'=> 'html','message'=> testPost })
     begin
-      RestClient.post(endpointURL, pmpayload, @headers)
+      RestClient.post(url, payload, @headers)
     rescue RestClient::ExceptionWithResponse => e
       puts(e.response)
     end
   end
-
+  
+  def sendRoomNotification(endpointURL, room, message)
+      url = endpointURL + room + "/notification"
+      testPost = message
+      payload = JSON.generate({ 'message_format'=> 'html','message'=> testPost })
+      begin
+        RestClient.post(url, payload, @headers)
+      rescue RestClient::ExceptionWithResponse => e
+        puts(e.response)
+      end
+    end
+    
   def getFoo(endPointURL)
-  kk = endPointURL
+    kk = endPointURL
     begin
-    RestClient.get(kk, @headers)
+      JSON.parse(RestClient.get(kk, @headers), {:object_class => Hash})
     rescue RestClient::ExceptionWithResponse => e
       puts(e.response)
     end
   end
-
-  def buildStart(endpointURL, message, buildID)
-    pushNotification = "false"
-    color = "green"
-    noticePrefix = "NOTICE: Build " + buildID + " started.<br>"
-    self.privateMessage(endpointURL,message,color, pushNotification, noticePrefix)
-  end
-
-  def buildFinish(endpointURL, message, buildID)
-    pushNotification = "false"
-    color = "green"
-    noticePrefix = "NOTICE: Build " + buildID + " finished.<br>"
-    self.privateMessage(endpointURL,message,color, pushNotification, noticePrefix)
-  end
-
-  def buildError(endpointURL, message, buildID)
-    pushNotification = "true"
-    color = "red"
-    noticePrefix = "ERROR: Build " + buildID + " failed.<br>"
-    self.privateMessage(endpointURL,message,color, pushNotification, noticePrefix)
-  end
-
 end
--- firstPost.rb
+++ firstPost.rb
@@ -1,46 +1,32 @@
 require "HipchatNotifications"
 
 # A personal auth token will result in the posts coming from your account.
-# https://encoretech.hipchat.com/account/api is the page where you generate such a token.
+# https://api.hipchat.com/account/api is the page where you generate such a token.
 token = File.read("credentials.txt") #one-line file containing the personal auth token
 
 # /v2/user/{id_or_email}/message is the API endpoint for a private message, requiring a different scope from rooms.
-# pmurl = File.read("pmUser.txt") #one-line file containing the target URL, for API private message to user
+pmurl = File.read("pmUser.txt") #one-line file containing the target URL, for API private message to user
 testurl = File.read("host.txt")
-
 getTestURL = File.read("hist.txt")
-
-vmBuildID = "9001"
-procQuant = "1"
-memQuant = "8 GB"
-OS = "CentOS"
-storage = "200 GB"
-transit = "1 TB"
-customer = "Taylor Swift"
-engineerContact = "Wait, Tay has an account with us?"
-currentTime = Time.now
-
-verboseTestMessage = ["<br> VM Resources <br> ",
-  "Cores: " + procQuant + "<br>", 
-  "Memory: " + memQuant + "<br>",
-  "OS: " + OS + "<br>",
-  "Storage: " + storage + "<br>",
-  "Transit: " + transit + "<br>",
-  "Customer: " + customer + "<br>",
-  "Account Engineer: " + engineerContact + "<br>",
-  "Build started at: " + currentTime.inspect + "<br>"].join
-
-testMessage = "This is an automated message testing color."
-finishMessage = "This is what it looks like when a build finishes."
-errorMessage = "This is what happens when something goes wrong."
 
 testObjectGet = HipchatNotifications.new(token)
 
 begin
- # HipchatBuildNotices.buildStart(token, testurl, verboseTestMessage, vmBuildID)
- # HipchatBuildNotices.buildFinish(token, testurl, verboseTestMessage, vmBuildID)
- # HipchatBuildNotices.buildError(token, testurl, verboseTestMessage, vmBuildID)
-  puts(testObjectGet.getFoo(getTestURL))
+  puts("Enter recipient: ")
+  recipient = gets.chomp
+  puts("Enter message to send: ")
+  message = gets.chomp
+  testObjectGet.sendMessage(pmurl,recipient,message)
+  rooms = testObjectGet.getFoo(getTestURL)
+  rooms.each do |room|
+    puts(room)
+    if (room.is_a?(Array))
+      puts(room.to_s + " is an Array.")
+    end
+    if (room.is_a?(Hash))
+      puts(room[:name])
+    end
+  end
 rescue RestClient::ExceptionWithResponse => e
   puts(e.response)
 end
